0780d2b7f5a3bc5cb7088ee608db33ae8be418fc
Edson <ed_edsoon@hotmail.com>
2017-05-26 22:28:32 -0300

    first cmt

---
+++ viterbi.c
@@ -0,0 +1,45 @@
+#include "viterbi.h"
+
+table_t *tbl = NULL;
+//considera o indice para os valores de 0 a 3
+//cada valor possui duas posições consecutivas para 0 e 1.
+// no vetor:
+//		00 :v [0] v[1]
+//    01 : v[2] v[3]
+//    10 : v[4] v[5]
+//    11 : v[6] v[7]
+int *v = (int[8]){00,11,11,00,10,01,01,10}; //val emitido
+int *v2 = (int[8]){00,10,00,10,01,11,01,11};	//val proximo
+
+
+void genTableValues(){
+
+	tbl = (table_t *)malloc(sizeof(table_t));
+	
+	tbl->valEmit = v;
+	tbl->valProx = v2;
+
+}
+
+
+int main(){
+	int i = 0;
+	
+	genTableValues();
+	//lê tamanho da entrada
+	scanf("%d", &tam);
+	
+	//le entrada
+	input = (int *)malloc(sizeof(int)*tam);
+	while(i < tam){	
+		scanf("%d", &input[i++]);
+	}
+	//for(i = 0; i < tam;i++)
+	//	printf("%d ", input[i]);
+	//for(i = 0; i < 8;i+=2){
+	//	printf("%d %d \n", tbl->valEmit[i],tbl->valEmit[i+1]);
+	//	printf("%d %d \n", tbl->valProx[i],tbl->valProx[i+1]);
+	//}
+	
+	return 0;
+}
---
+++ viterbi.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct tb{
+
+	int *valEmit;
+	int *valProx;
+
+}table_t;
+
+int tam;
+int *input;
